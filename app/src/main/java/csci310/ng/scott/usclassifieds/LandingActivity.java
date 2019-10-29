512987d4268b2488c1888879ff1033712e4feccb
Scott Ng <scottng@usc.edu>
2019-10-29 12:00:13 -0700

    Initial commit

---
+++ app/src/main/java/csci310/ng/scott/usclassifieds/LandingActivity.java
@@ -0,0 +1,20 @@
+package csci310.ng.scott.usclassifieds;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
+import androidx.appcompat.widget.SwitchCompat;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class LandingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+        setContentView(R.layout.activity_landing);
+    }
+
+}
---
+++ app/src/main/java/csci310/ng/scott/usclassifieds/LoginActivity.java
@@ -0,0 +1,15 @@
+package csci310.ng.scott.usclassifieds;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+
+}
