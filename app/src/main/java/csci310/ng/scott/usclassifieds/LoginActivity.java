232f6956838dd58c54a4835a46ac3ea98f4dbaf9
Scott Ng <scottng@usc.edu>
2019-10-30 10:25:33 -0700

    Change string resources

--- app/src/main/java/csci310/ng/scott/usclassifieds/LoginActivity.java
+++ app/src/main/java/csci310/ng/scott/usclassifieds/LoginActivity.java
@@ -1,18 +1,50 @@
 package csci310.ng.scott.usclassifieds;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class LoginActivity extends AppCompatActivity {
 
     private static final String TAG = "LoginActivity";
     public static final int REQUEST_CODE = 1;
 
+    private FirebaseAuth mAuth;
+
+    EditText editTextEmail;
+    EditText editTextPassword;
+    Button buttonLogin;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        editTextEmail = findViewById(R.id.edit_text_email);
+        editTextPassword = findViewById(R.id.edit_text_password);
+        buttonLogin = findViewById(R.id.button_login);
+
+        buttonLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+            }
+        });
     }
+
+
 
 }
