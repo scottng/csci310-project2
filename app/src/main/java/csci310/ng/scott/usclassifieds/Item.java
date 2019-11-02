b442ad02bddef2dd77877d1989786b8acfbfb74e
Scott Ng <scottng@usc.edu>
2019-11-02 14:35:57 -0700

    Add ItemAdapter to MarketActivity

---
+++ app/src/main/java/csci310/ng/scott/usclassifieds/Item.java
@@ -0,0 +1,81 @@
+package csci310.ng.scott.usclassifieds;
+
+public class Item {
+    private String itemID;
+    private String sellerID;
+    private String title;
+    private String description;
+    private double price;
+    private Boolean sold;
+    private String photoURL;
+
+    public Item() {
+
+    }
+
+    public Item(String itemID, String sellerID, String title, String description, double price, Boolean sold, String photoURL) {
+        this.itemID = itemID;
+        this.sellerID = sellerID;
+        this.title = title;
+        this.description = description;
+        this.price = price;
+        this.sold = sold;
+        this.photoURL = photoURL;
+    }
+
+    public String getItemID() {
+        return itemID;
+    }
+
+    public void setItemID(String itemID) {
+        this.itemID = itemID;
+    }
+
+    public String getSellerID() {
+        return sellerID;
+    }
+
+    public void setSellerID(String sellerID) {
+        this.sellerID = sellerID;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public Boolean getSold() {
+        return sold;
+    }
+
+    public void setSold(Boolean sold) {
+        this.sold = sold;
+    }
+
+    public String getPhotoURL() {
+        return photoURL;
+    }
+
+    public void setPhotoURL(String photoURL) {
+        this.photoURL = photoURL;
+    }
+}
--- app/src/main/java/csci310/ng/scott/usclassifieds/MarketActivity.java
+++ app/src/main/java/csci310/ng/scott/usclassifieds/MarketActivity.java
@@ -1,5 +1,6 @@
 package csci310.ng.scott.usclassifieds;
 
+import android.content.Context;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -9,7 +10,13 @@ import com.google.android.material.bottomnavigation.BottomNavigationView;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.GridView;
 import android.widget.TextView;
+
+import java.util.List;
 
 public class MarketActivity extends AppCompatActivity {
 
@@ -17,6 +24,9 @@ public class MarketActivity extends AppCompatActivity {
     public static final int REQUEST_CODE = 3;
 
     private TextView mTextMessage;
+    private GridView gridViewItems;
+
+    private ItemAdapter adapter;
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -24,14 +34,14 @@ public class MarketActivity extends AppCompatActivity {
         @Override
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             switch (item.getItemId()) {
-                case R.id.navigation_home:
-                    mTextMessage.setText(R.string.title_home);
+                case R.id.navigation_market:
+                    mTextMessage.setText(R.string.title_market);
                     return true;
-                case R.id.navigation_dashboard:
-                    mTextMessage.setText(R.string.title_dashboard);
-                    return true;
-                case R.id.navigation_notifications:
+                case R.id.navication_notifications:
                     mTextMessage.setText(R.string.title_notifications);
+                    return true;
+                case R.id.navigation_profile:
+                    mTextMessage.setText(R.string.title_profile);
                     return true;
             }
             return false;
@@ -43,9 +53,47 @@ public class MarketActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_market);
 
+        // Link bottom navigation
         mTextMessage = (TextView) findViewById(R.id.message);
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
+        // Link UI elements
+        gridViewItems = findViewById(R.id.grid_items);
+
+
+    }
+
+    private class ItemAdapter extends ArrayAdapter<Item> {
+        private List<Item> items;
+
+        public ItemAdapter(Context context, int resource, List<Item> objects){
+            super(context, resource, objects);
+            this.items = objects;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            if(convertView == null) {
+                convertView = getLayoutInflater().inflate(R.layout.layout_item, null);
+            }
+
+            Item curr = items.get(position);
+
+            TextView textTitle = convertView.findViewById(R.id.list_item_name);
+            TextView textPrice = convertView.findViewById(R.id.list_item_price);
+            TextView textDistance = convertView.findViewById(R.id.list_item_distance);
+
+            textTitle.setText(curr.getTitle());
+            textPrice.setText(Double.toString(curr.getPrice()));
+
+            return convertView;
+        }
+
+//        @Override
+//        public long getItemId(int position) {
+//            return getItem(position).getId();
+//        }
     }
 
 }
