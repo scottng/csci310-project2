b3139ff291436cc0a8bd3b44f490557aff2b93e6
Ambrish Parekh <parekha@usc.edu>
2019-11-02 22:50:24 -0700

    Add Firebase User creation

---
+++ app/src/main/java/csci310/ng/scott/usclassifieds/User.java
@@ -0,0 +1,69 @@
+package csci310.ng.scott.usclassifieds;
+
+public class User {
+    private String profilePic;
+    private String fullName;
+    private String email;
+    private String phone;
+    private String userID;
+    private String textBio;
+
+    public User() {}
+
+    public User(String fullName, String email, String textBio) {
+        this.profilePic = "";
+        this.fullName = fullName;
+        this.email = email;
+        this.phone = "";
+        this.userID = "";
+        this.textBio = textBio;
+    }
+
+    public String getProfilePic() {
+        return profilePic;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public String getUserID() {
+        return userID;
+    }
+
+    public String getTextBio() {
+        return textBio;
+    }
+
+    public void setProfilePic(String profilePic) {
+        this.profilePic = profilePic;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public void setUserID(String userID) {
+        this.userID = userID;
+    }
+
+    public void setTextBio(String textBio) {
+        this.textBio = textBio;
+    }
+}
