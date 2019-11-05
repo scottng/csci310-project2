365b52e249f1fde07f516cd9365aecc7893bf75b
Addison Allred <addisonallred@Addisons-MacBook-Pro.local>
2019-11-05 13:43:30 -0800

    finished the user filter

---
+++ app/src/main/java/csci310/ng/scott/usclassifieds/UserListAdapter.java
@@ -0,0 +1,40 @@
+package csci310.ng.scott.usclassifieds;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import com.bumptech.glide.Glide;
+
+import java.util.List;
+
+public class UserListAdapter extends ArrayAdapter<User> {
+    private int mresource;
+    private Context mcontext;
+    public UserListAdapter(@NonNull Context context, int resource, @NonNull List<User> objects) {
+        super(context, resource, objects);
+        mresource = resource;
+        mcontext = context;
+    }
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+
+        String img = getItem(position).getProfilePic();
+        String fullName = getItem(position).getFullName();
+
+        LayoutInflater inflater = LayoutInflater.from(mcontext);
+        convertView = inflater.inflate(mresource, parent, false);
+        TextView nameView = (TextView) convertView.findViewById(R.id.user_full_name);
+        Glide.with(mcontext).load(img).into((ImageView) convertView.findViewById(R.id.image_item));
+        nameView.setText(fullName);
+        return convertView;
+    }
+}
