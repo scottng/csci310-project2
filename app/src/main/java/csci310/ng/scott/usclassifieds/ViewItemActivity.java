7916e8c4d693a886613df822236645d81253b140
Scott Ng <scottng@usc.edu>
2019-11-05 02:43:08 -0800

    Add big red button to activity_view_item.xml

--- app/src/main/java/csci310/ng/scott/usclassifieds/ViewItemActivity.java
+++ app/src/main/java/csci310/ng/scott/usclassifieds/ViewItemActivity.java
@@ -2,13 +2,38 @@ package csci310.ng.scott.usclassifieds;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 
 public class ViewItemActivity extends Activity {
 
+    // UI Elements
+    ImageView viewItemImage;
+    TextView viewItemName;
+    TextView viewItemPrice;
+    TextView viewItemDescription;
+    ImageView viewProfilePicture;
+    TextView textProfileName;
+    Button buttonMarkSold;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        // Link UI
+        viewItemImage = findViewById(R.id.view_item_img);
+        viewItemName = findViewById(R.id.view_item_name);
+        viewItemPrice = findViewById(R.id.view_item_price);
+        viewItemDescription = findViewById(R.id.view_item_description);
+        viewProfilePicture = findViewById(R.id.view_profile_picture);
+        textProfileName = findViewById(R.id.text_profile_name);
+        buttonMarkSold = findViewById(R.id.button_mark_sold);
+
+        // YOU CAN SET THIS BUTTON'S VISIBILITY
+        buttonMarkSold.setVisibility(View.VISIBLE);
     }
 }
