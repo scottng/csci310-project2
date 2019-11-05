f4cc2286640916e83b69edda71d9bc66f2fd9187
Addison Allred <addisonallred@usc-guestwireless-upc-newsc21618.usc.edu>
2019-11-04 23:10:26 -0800

    removed items

--- app/src/main/java/csci310/ng/scott/usclassifieds/OtherProfileActivity.java
+++ app/src/main/java/csci310/ng/scott/usclassifieds/OtherProfileActivity.java
@@ -18,13 +18,8 @@ public class OtherProfileActivity extends Activity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         ListView list = (ListView) findViewById(R.id.list_items_in_other_profile);
-        Item item1 = new Item("1", "1", "bike", "this is my bike", 34.43, false, "https://slack-redir.net/link?url=https%3A%2F%2Ffirebasestorage.googleapis.com%2Fv0%2Fb%2Fusclassifieds-1ac3c.appspot.com%2Fo%2Fitem_pics%252FBdy4EvB4b4eEhPFxNjqCYNX07eV2-1572925402757%3Falt%3Dmedia%26token%3D3774fe4a-05fe-487c-9ab6-8e7d64864129");
-        Item item2 = new Item("2", "2", "dog", "this is my dog", 1, false, "https://slack-redir.net/link?url=https%3A%2F%2Ffirebasestorage.googleapis.com%2Fv0%2Fb%2Fusclassifieds-1ac3c.appspot.com%2Fo%2Fitem_pics%252FBdy4EvB4b4eEhPFxNjqCYNX07eV2-1572925402757%3Falt%3Dmedia%26token%3D3774fe4a-05fe-487c-9ab6-8e7d64864129");
-        Item item3 = new Item("3", "3", "cat", "this is my cat", 0.5, false, "https://slack-redir.net/link?url=https%3A%2F%2Ffirebasestorage.googleapis.com%2Fv0%2Fb%2Fusclassifieds-1ac3c.appspot.com%2Fo%2Fitem_pics%252FBdy4EvB4b4eEhPFxNjqCYNX07eV2-1572925402757%3Falt%3Dmedia%26token%3D3774fe4a-05fe-487c-9ab6-8e7d64864129");
-        ArrayList<Item> itemList = new ArrayList<Item>();
-        itemList.add(item1);
-        itemList.add(item2);
-        itemList.add(item3);
+         ArrayList<Item> itemList = new ArrayList<Item>();
+
         ItemListAdapter adapter = new ItemListAdapter(this, R.layout.layout_item, itemList);
         list.setAdapter(adapter);
     }
