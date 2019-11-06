1d1c4bb07202badbdb48ec191802bcd91ee6ee93
Ambrish Parekh <parekha@usc.edu>
2019-11-05 17:18:05 -0800

    add notifications from database

--- app/src/main/java/csci310/ng/scott/usclassifieds/NotificationsActivity.java
+++ app/src/main/java/csci310/ng/scott/usclassifieds/NotificationsActivity.java
@@ -6,17 +6,39 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class NotificationsActivity extends AppCompatActivity {
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener;
     private BottomNavigationView navigation;
 
+    // Firebase stuff
+    private FirebaseAuth firebaseAuth;
+    private FirebaseDatabase firebaseDatabase;
+    private FirebaseAuth.AuthStateListener authStateListener;
+
+    private List<Notification> notifications = new ArrayList<>();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        firebaseAuth = FirebaseAuth.getInstance();
+        firebaseDatabase = FirebaseDatabase.getInstance();
+
         setContentView(R.layout.activity_notifications);
         navigation = findViewById(R.id.navigationNote);
         mOnNavigationItemSelectedListener = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -41,6 +63,41 @@ public class NotificationsActivity extends AppCompatActivity {
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
         //set selected item depending on Activity
         navigation.setSelectedItemId(R.id.navigation_notifications);
+
+        authStateListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser currUser = firebaseAuth.getCurrentUser();
+                if (currUser == null) {
+                    Toast.makeText(NotificationsActivity.this, "Signed Out", Toast.LENGTH_SHORT).show();
+                    startActivity(new Intent(getApplicationContext(), LandingActivity.class));
+                }
+            }
+        };
+
+        DatabaseReference rootRef = firebaseDatabase.getReference().child("Notification");
+        rootRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                notifications.clear();
+
+                for (DataSnapshot dss : dataSnapshot.getChildren()) {
+                    Notification notification = dss.getValue(Notification.class);
+
+                    if (notification.getReceiverUid() == firebaseAuth.getCurrentUser().getUid()) {
+                        notifications.add(notification);
+                    }
+                }
+
+                // item adapter here
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
     }
 
     @Override
@@ -49,5 +106,17 @@ public class NotificationsActivity extends AppCompatActivity {
         navigation.setSelectedItemId(R.id.navigation_notifications);
     }
 
+    @Override
+    protected void onStart() {
+        super.onStart();
+        firebaseAuth.addAuthStateListener(authStateListener);
+    }
 
+    @Override
+    protected void onStop() {
+        super.onStop();
+        if(authStateListener != null) {
+            firebaseAuth.removeAuthStateListener(authStateListener);
+        }
+    }
 }
